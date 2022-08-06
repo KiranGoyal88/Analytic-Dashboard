b5e68a4e1ca810b498bff44dea24398ca094dc5e
Kiran Goyal <kiran.goyal@pixis.ai>
2022-08-06 13:50:00 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Grid } from '@mui/material';
 import './App.css';
+import { BarChartComponent } from './components/BarChartComponent';
+import { LineChartComponent } from './components/LineChartComponent';
+import TabComponent from './components/TabComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Google Analytics Reports</h2>
+      <TabComponent />
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <BarChartComponent />
+        </Grid>
+        <Grid item xs={6}>
+          <LineChartComponent />
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
